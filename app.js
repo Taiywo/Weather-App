75904cc501da48ae4a3a6354fc75facf1005f390
Taiywo <kareemtaiywo@gmail.com>
2023-05-24 14:10:58 +0100

    added more features

--- app.js
+++ app.js
@@ -5,37 +5,47 @@ const inputVal = document.getElementById('input')
 const btn = document.getElementById('btn')
 const weatherImg = document.getElementById('img')
 const locations = document.getElementById('locations')
- 
-console.log(input.value)
+
 
 
 btn.addEventListener('click', () => {
 
-  let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + inputVal.value + '&appid=6ec2215b6f05631de0ba19b062a81809'
-  let getAbuja = () => {
-    fetch(url).then(response => {
-      return response.json()
-    }).then(data => {
-      console.log(data.weather);
-      console.log(data.weather[0].description);
-      const description = data.weather[0].description
-      const weatherDegree = data.main.temp
-      weatherCelcius = Math.floor(weatherDegree - 273)
-      console.log(weatherDegree);
-      main.innerText = description
-      degree.innerText = ` ${weatherCelcius} °C`
-      let icon1 = data.weather[0].icon;
-    
-      console.log(icon1);
-      weatherImg.src = `http://openweathermap.org/img/w/` + icon1 + `.png`
-      locations.innerText = inputVal.value
-      date.innerHTML = time1
-      inputVal.value = ""
+  // Construct the URL for the OpenWeatherMap API
+  let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + inputVal.value + '&appid=6ec2215b6f05631de0ba19b062a81809';
 
-    })
-      .catch(err => alert('You entered Wrong city name'))
+  // Define a function to fetch weather data
+  let weatherData = () => {
+    // Make the API request
+    fetch(url)
+      .then(response => {
+        // Check if the response status is 404 (Not Found)
+        if (response.status === 404) {
+          throw new Error('Server Error: ' + response.status);
+        }
+        // Convert the response to JSON
+        return response.json();
+      })
+      .then(data => {
+        // Extract the necessary data from the response
+        const description = data.weather[0].description;
+        const weatherDegree = data.main.temp;
+        const weatherCelcius = Math.floor(weatherDegree - 273);
+
+        // Display the weather information on the webpage
+        main.innerText = description;
+        degree.innerText = ` ${weatherCelcius} °C`;
+        let icon1 = data.weather[0].icon;
+        weatherImg.src = `http://openweathermap.org/img/w/` + icon1 + `.png`;
+        locations.innerText = inputVal.value;
+        inputVal.value = "";
+      })
+      .catch(error => {
+        // Handle the error by displaying an alert message
+        alert("Wrong city, please try again");
+        inputVal.value = "";
+      });
   }
-  getAbuja()
-})
-let ghh = new Date().toLocaleString("en-US", {timeZone: "africa/lagos"})
-console.log(ghh);
+
+  // Call the weatherData function to initiate the API request
+  weatherData();
+});
