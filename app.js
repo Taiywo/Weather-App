9c804a9fc07741b524cdb9ea4dedb68081ea5c09
Taiywo <54299107+Taiywo@users.noreply.github.com>
2023-06-01 17:19:38 +0100

    redesign the ui and improved the ux

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
+const card = document.getElementById('card')
 const degree = document.getElementById('degree')
-const main = document.getElementById('main')
+const main = document.getElementById('descrip')
 const date = document.getElementById('date')
 const inputVal = document.getElementById('input')
-const btn = document.getElementById('btn')
+const minmax = document.getElementById('degree2')
 const weatherImg = document.getElementById('img')
 const locations = document.getElementById('locations')
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0]
+const today = new Date();
+
+const formattedDate = today.toDateString();
+
+// const options = { month: 'long', year: 'numeric' };
 
 
-
-btn.addEventListener('click', () => {
+inputVal.addEventListener('change', () => {
 
   // Construct the URL for the OpenWeatherMap API
-  let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + inputVal.value + '&appid=6ec2215b6f05631de0ba19b062a81809';
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + inputVal.value + '&appid=6ec2215b6f05631de0ba19b062a81809';
 
   // Define a function to fetch weather data
-  let weatherData = () => {
+  const weatherData = () => {
     // Make the API request
     fetch(url)
       .then(response => {
@@ -26,26 +33,40 @@ btn.addEventListener('click', () => {
         return response.json();
       })
       .then(data => {
+        console.log(data);
         // Extract the necessary data from the response
         const description = data.weather[0].description;
         const weatherDegree = data.main.temp;
         const weatherCelcius = Math.floor(weatherDegree - 273);
+        const weatherMin = Math.floor(data.main.temp_min-273)
+        const weatherMax = Math.floor(data.main.temp_max-273)
 
         // Display the weather information on the webpage
         main.innerText = description;
-        degree.innerText = ` ${weatherCelcius} °C`;
+        degree.innerText = ` ${weatherCelcius}°`;
+
         let icon1 = data.weather[0].icon;
         weatherImg.src = `http://openweathermap.org/img/w/` + icon1 + `.png`;
+
         locations.innerText = inputVal.value;
         inputVal.value = "";
+
+        date.innerText = formattedDate
+
+        minmax.innerText = `${weatherMin}/${weatherMax}`
+
       })
       .catch(error => {
         // Handle the error by displaying an alert message
-        alert("Wrong city, please try again");
+        window.Error(modal.style.display = "block");
+        span.onclick = function () {
+          modal.style.display = "none";
+        }
         inputVal.value = "";
       });
   }
 
   // Call the weatherData function to initiate the API request
   weatherData();
+  card.style.display = 'flex'
 });
